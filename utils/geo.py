a025da56c972e7739f42352b33e85719c68e77f2
Juan Pablo Carrillo Hortua <juanpablocarrillohortua@gmail.com>
2026-08-13 16:08:44 -0500

    eda: MAR diagnosis and drop atipic prices

--- utils/geo.py
+++ utils/geo.py
@@ -44,6 +44,16 @@ def inside_bbox_mask(
         bbox.lon_min, bbox.lon_max
     )
 
+def single_bbox_inside(
+        lat: int,
+        lon:int,
+        bbox: BBox
+        ) -> bool:
+
+    """Check a single one coordinate"""
+
+    return (bbox.lat_min < lat < bbox.lat_max) and (bbox.lon_min < lon < bbox.lon_max)
+
 
 def outside_bbox_mask(
     lat: pd.Series,
